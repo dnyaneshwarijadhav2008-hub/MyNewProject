e983ae8a58fccc9fa3c15e56e5ff3d4518421eee
Dnyanu <dnyaneshwarijadhav2850@gmail.com>
2026-05-17 17:26:50 +0530

    Code to check even or odd

---
+++ even or odd.py
@@ -0,0 +1,9 @@
+# write a function to check the number is even or odd
+def even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+print(even_odd(10))  
+print(even_odd(7))   
